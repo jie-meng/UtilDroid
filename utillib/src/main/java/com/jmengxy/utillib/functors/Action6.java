c4a28dac7041ae78d6d0497241eddea01fe457ff
Jie Meng <jiemeng@thoughtworks.com>
2018-02-10 22:54:19 +0800

    Add more functors

---
+++ utillib/src/main/java/com/jmengxy/utillib/functors/Action6.java
@@ -0,0 +1,9 @@
+package com.jmengxy.utillib.functors;
+
+/**
+ * Created by jiemeng on 10/02/2018.
+ */
+
+public interface Action6<T1, T2, T3, T4, T5, T6> {
+    void apply(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6);
+}
---
+++ utillib/src/main/java/com/jmengxy/utillib/functors/Func6.java
@@ -0,0 +1,9 @@
+package com.jmengxy.utillib.functors;
+
+/**
+ * Created by jiemeng on 29/09/2017.
+ */
+
+public interface Func6<T1, T2, T3, T4, T5, T6, R> {
+    R apply(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6);
+}
